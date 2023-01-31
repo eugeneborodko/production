d7503d3d9d178435bc3a07cae72b7501f356804b
Yauheni Baradzko <Yauheni.Baradzko@itechart-group.com>
2023-01-31 19:29:03 +0100

    feat: add css variables, add light and dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Suspense } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
-import { AboutPageLazy } from '../pages/AboutPage/AboutPage.lazy'
-import { MainPageLazy } from '../pages/MainPage/MainPage.lazy'
-import './index.scss'
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
+import { useTheme } from './theme/useTheme'
+import './styles/index.scss'
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to="/">Main page</Link>
       <Link to="/about">About page</Link>
+      <button onClick={toggleTheme}>Change theme</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/about" element={<AboutPageLazy />} />
