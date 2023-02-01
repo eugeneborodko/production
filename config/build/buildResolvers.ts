19f5e79ea89b0b00f574c2e42c861e6c49183047
Yauheni Baradzko <Yauheni.Baradzko@itechart-group.com>
2023-02-01 19:49:22 +0100

    feat: apply feature-sliced architectural methodology, configure absolute imports

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
 import { ResolveOptions } from 'webpack'
+import { BuildOptions } from './types/config'
 
-export const buildResolvers = (): ResolveOptions => {
+export const buildResolvers = (options: BuildOptions): ResolveOptions => {
   return {
     extensions: ['.tsx', '.ts', '.js'],
+    preferAbsolute: true,
+    mainFiles: ['index'],
+    modules: [options.paths.src, 'node_modules'],
+    alias: {}
   }
 }
