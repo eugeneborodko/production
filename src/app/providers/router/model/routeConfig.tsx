d67f5cb714885f702148d909574e278ea8be5844
Yauheni Baradzko <Yauheni.Baradzko@itechart-group.com>
2023-02-06 14:21:47 +0100

    refactor: improve router config path field

--- src/app/providers/router/model/routeConfig.tsx
+++ src/app/providers/router/model/routeConfig.tsx
@@ -1,15 +1,15 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { RouteProps } from 'react-router-dom';
-import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
+import { RouteProps } from 'react-router-dom'
+import { AppRoutes, RoutePath } from 'shared/config/routeConfig/routeConfig'
 
 export const routeConfig: RouteProps[] = [
   {
-    path: RoutePath.main,
+    path: RoutePath[AppRoutes.MAIN],
     element: <MainPage />,
   },
   {
-    path: RoutePath.about,
+    path: RoutePath[AppRoutes.ABOUT],
     element: <AboutPage />,
   },
 ]
