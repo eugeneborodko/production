c549ddb27a4c31ce317c96cbf0efd873682603dd
Yauheni Baradzko <Yauheni.Baradzko@itechart-group.com>
2023-03-06 19:29:26 +0100

    feat: add story for modal, add initial theme for ThemeProvider

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -5,10 +5,14 @@ import {
   ThemeContext,
 } from '../lib/ThemeContext';
 
+interface ThemeProviderProps {
+  initialTheme?: Theme
+}
+
 const defaultTheme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
 
-const ThemeProvider: FC = ({ children }) => {
-  const [theme, setTheme] = useState<Theme>(defaultTheme);
+const ThemeProvider: FC<ThemeProviderProps> = ({ initialTheme, children }) => {
+  const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
   const defaultProps = useMemo(
     () => ({
---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Modal } from './Modal';
+
+export default {
+  title: 'shared/Modal',
+  component: Modal,
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => (
+  <Modal {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  isOpened: true,
+  children: 'Modal',
+};
